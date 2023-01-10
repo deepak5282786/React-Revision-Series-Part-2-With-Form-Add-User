9c1aaa2ba9ce59f48b3115d094d8acdb0d6a5df3
Deepak Yadav <deepak5282786@gmail.com>
2023-01-10 08:51:14 +0530

    Adding feature of React Portal to which provide way to render children components into a DOM node which typically occurs outside the DOM hierarchy of the parent component.

--- src/UI/ErrorModal.js
+++ src/UI/ErrorModal.js
@@ -2,23 +2,44 @@ import React from "react";
 import Button from "./Button";
 import { Card } from "./Card";
 import classes from "./ErrorModal.module.css";
+import ReactDom from "react-dom";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <div className={classes.header}>
+        <header>
+          <h2>{props.title}</h2>
+        </header>
+      </div>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <div className={classes.actions}>
+        <Button onClick={props.onOkayHandler}>Okay</Button>
+      </div>
+    </Card>
+  );
+};
 export const ErrorModal = (props) => {
   return (
-    <div className={classes.backdrop} onClick={props.onOkayHandler}>
-      <Card className={classes.modal}>
-        <div className={classes.header}>
-          <header>
-            <h2>{props.title}</h2>
-          </header>
-        </div>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <div className={classes.actions}>
-          <Button onClick={props.onOkayHandler}>Okay</Button>
-        </div>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onClick={props.onOkayHandler} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onOkayHandler={props.onOkayHandler}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
